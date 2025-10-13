63f0a8ac40f97d237e7116354d1bb3af90ead612
Saša Barišić <sbarisic@vub.hr>
2025-10-13 02:17:06 +0200

    Fixes print function and variable addressing
    Addresses issues in the print function related to incorrect
    variable addressing and loop conditions.
    
    Removes unused variable definitions and adjusts memory offsets
    to ensure proper data access during program execution.
    
    Updates string literal references.

--- Fishmachine/ct.asm
+++ Fishmachine/ct.asm
@@ -1,13 +1,6 @@
 # VariableDef BEGIN - int_table
     .globl int_table
 # VariableDef END - int_table
-# VariableDef BEGIN - temp_buffer
-    temp_buffer:
-# VariableDef END - temp_buffer
-# VariableAssign BEGIN
-    .STATVAR_0001:
-    .String "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-# VariableAssign END
 .globl print
 print:
     PUSH_REG %ebp
@@ -17,12 +10,12 @@ print:
     # VariableDef END - i
     # VariableAssign BEGIN
         MOVE_LONG_REG $0, %eax
-        MOVE_REG_OFFSET_REG %eax, -8, %ebp
+        MOVE_REG_OFFSET_REG %eax, -4, %ebp
     # VariableAssign END
     # While BEGIN
-        .WHILE_0004:
+        .WHILE_0002:
         # IndexOp BEGIN
-            MOVE_OFFSET_REG_REG -8, %ebp, %eax
+            MOVE_OFFSET_REG_REG -4, %ebp, %eax
             MOVE_REG_REG %eax, %ebx
             MOVE_OFFSET_REG_REG 8, %ebp, %eax
             ADD_REG_REG %ebx, %eax
@@ -31,10 +24,10 @@ print:
         MOVE_REG_REG %eax, %ebx
         MOVE_LONG_REG $0, %eax
         CMP_REG_REG %eax, %ebx
-        JUMP_IF_ZERO_LONG .ENDWHILE_0006
+        JUMP_IF_ZERO_LONG .ENDWHILE_0004
         # syscall_2 BEGIN
             # IndexOp BEGIN
-                MOVE_OFFSET_REG_REG -8, %ebp, %eax
+                MOVE_OFFSET_REG_REG -4, %ebp, %eax
                 MOVE_REG_REG %eax, %ebx
                 MOVE_OFFSET_REG_REG 8, %ebp, %eax
                 ADD_REG_REG %ebx, %eax
@@ -47,15 +40,15 @@ print:
         # syscall_2 END
         # MathOp BEGIN (+)
             PUSH_REG %ebx
-            MOVE_OFFSET_REG_REG -8, %ebp, %eax
+            MOVE_OFFSET_REG_REG -4, %ebp, %eax
             MOVE_REG_REG %eax, %ebx
             MOVE_LONG_REG $1, %eax
             ADD_REG_REG %ebx, %eax
             POP_REG %ebx
         # MathOp END (+)
-        MOVE_REG_OFFSET_REG %eax, -8, %ebp
-        JUMP_LONG .WHILE_0004
-        .ENDWHILE_0006:
+        MOVE_REG_OFFSET_REG %eax, -4, %ebp
+        JUMP_LONG .WHILE_0002
+        .ENDWHILE_0004:
     # While END
     LEAVE 
     RET 
@@ -64,7 +57,7 @@ handler_int0:
     PUSH_REG %ebp
     MOVE_REG_REG %esp, %ebp
     # FuncCall BEGIN - print
-        MOVE_LONG_REG .L_0009, %eax
+        MOVE_LONG_REG .L_0007, %eax
         PUSH_REG %eax
         MOVE_LONG_REG print, %eax
         CALL_REG %eax
@@ -86,14 +79,14 @@ handler_int2_keyboardchar:
         SUB_LONG_REG $4, %esp
     # VariableDef END - tst
     # VariableAssign BEGIN
-        MOVE_LONG_REG .L_000C, %eax
-        MOVE_REG_OFFSET_REG %eax, -12, %ebp
+        MOVE_LONG_REG .L_000A, %eax
+        MOVE_REG_OFFSET_REG %eax, -8, %ebp
     # VariableAssign END
     # Expr_AssignValue BEGIN
         # IndexOp BEGIN
             MOVE_LONG_REG $0, %eax
             MOVE_REG_REG %eax, %ebx
-            MOVE_OFFSET_REG_REG -12, %ebp, %eax
+            MOVE_OFFSET_REG_REG -8, %ebp, %eax
             ADD_REG_REG %ebx, %eax
         # IndexOp END
         MOVE_REG_REG %eax, %ebx
@@ -105,7 +98,7 @@ handler_int2_keyboardchar:
         # IndexOp BEGIN
             MOVE_LONG_REG $1, %eax
             MOVE_REG_REG %eax, %ebx
-            MOVE_OFFSET_REG_REG -12, %ebp, %eax
+            MOVE_OFFSET_REG_REG -8, %ebp, %eax
             ADD_REG_REG %ebx, %eax
         # IndexOp END
         MOVE_REG_REG %eax, %ebx
@@ -117,7 +110,7 @@ handler_int2_keyboardchar:
         # IndexOp BEGIN
             MOVE_LONG_REG $2, %eax
             MOVE_REG_REG %eax, %ebx
-            MOVE_OFFSET_REG_REG -12, %ebp, %eax
+            MOVE_OFFSET_REG_REG -8, %ebp, %eax
             ADD_REG_REG %ebx, %eax
         # IndexOp END
         MOVE_REG_REG %eax, %ebx
@@ -126,7 +119,7 @@ handler_int2_keyboardchar:
         MOVE_REG_OFFSET_REG %eax, 0, %ebx
     # Expr_AssignValue END
     # FuncCall BEGIN - print
-        MOVE_OFFSET_REG_REG -12, %ebp, %eax
+        MOVE_OFFSET_REG_REG -8, %ebp, %eax
         PUSH_REG %eax
         MOVE_LONG_REG print, %eax
         CALL_REG %eax
@@ -190,24 +183,24 @@ kmain:
         MOVE_REG_OFFSET_REG %eax, 0, %ebx
     # Expr_AssignValue END
     # FuncCall BEGIN - print
-        MOVE_LONG_REG .L_000E, %eax
+        MOVE_LONG_REG .L_000C, %eax
         PUSH_REG %eax
         MOVE_LONG_REG print, %eax
         CALL_REG %eax
         ADD_LONG_REG $4, %esp
     # FuncCall END - print
     # While BEGIN
-        .WHILE_000F:
+        .WHILE_000D:
         NOP 
-        JUMP_LONG .WHILE_000F
-        .ENDWHILE_0011:
+        JUMP_LONG .WHILE_000D
+        .ENDWHILE_000F:
     # While END
     SYSCALL $0
     LEAVE 
     RET 
-.L_0009:
+.L_0007:
     .String "INT0\n"
-.L_000C:
+.L_000A:
     .String "Hello"
-.L_000E:
+.L_000C:
     .String "Hello, World!\n"
